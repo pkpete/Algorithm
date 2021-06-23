f616e94eaa360695f992c2c0f569ebcb960ee7ab
pkpete <pkpete@naver.com>
2021-06-23 23:28:54 +0900

    20210623

--- 백준 알고리즘/2667.cpp
+++ 백준 알고리즘/2667.cpp
@@ -1,48 +1,56 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
-int map[26][26];
-int check[26][26];
-int cnt = 0;
-vector<int> arr;
 
-void dfs(int y, int x){
-	check[y][x] = cnt;
-	arr[cnt]++;
-	if(map[y+1][x] == 1 && check[y+1][x] == 0)
-		dfs(y+1,x);
-	if(map[y-1][x] == 1 && check[y-1][x] == 0)
-		dfs(y-1,x);
-	if(map[y][x+1] == 1 && check[y][x+1] == 0)
-		dfs(y,x+1);
-	if(map[y][x-1] == 1 && check[y][x-1] == 0)
-		dfs(y,x-1);
+int map[26][26], n;
+bool check[26][26];
+int dirX[4] = { 1,0,-1,0 };
+int dirY[4] = { 0,1,0,-1 };
+vector<int> houses;
+
+void input() {
+	string str;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> str;
+		for (int j = 0; j < n; j++) {
+			map[i][j] = str[j] - '0';
+		}
+	}
 }
 
-int main(){
-	int n;
-	string s;
-	cin >> n;
-	for(int i = 1; i <= n; i++){
-		cin >> s;
-		for(int j = 0; j < s.length(); j++){
-			map[i][j+1] = s[j] - '0';
+void dfs(int y, int x, int &cnt) {
+	cnt++;
+	check[y][x] = true;
+	for (int i = 0; i < 4; i++) {
+		int movex = x + dirX[i];
+		int movey = y + dirY[i];
+		if (movex >= 0 && movey >= 0 && movex < n && movey < n && map[movey][movex] == 1 && !check[movey][movex]) {
+			dfs(movey, movex, cnt);
 		}
 	}
-	arr.push_back(0);
-	for(int i = 1; i <= n; i++){
-		for(int j = 1; j <= n; j++){
-			if(map[i][j] == 1 && check[i][j] == 0){
-				cnt++;
-				arr.push_back(0);
-				dfs(i,j);
+
+}
+
+void solve() {
+	for (int i = 0; i < n; i++) 
+		for (int j = 0; j < n; j++) 
+			if (map[i][j] == 1 && !check[i][j]) {
+				int cnt = 0;
+				dfs(i, j, cnt);
+				houses.push_back(cnt);
 			}
-		}
-	}
-	cout << cnt << endl;
-	sort(arr.begin(), arr.end());
-	for(int i = 1; i <= cnt; i++)
-		cout << arr[i] << endl;
+}
+
+int main() {
+	
+	input();
+	solve();
+	sort(houses.begin(), houses.end());
+
+	cout << houses.size() << endl;
+	for (int i = 0; i < houses.size(); i++)
+		cout << houses[i] << endl;
 }
