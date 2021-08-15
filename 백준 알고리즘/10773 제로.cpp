b0d818a32c45468cebc155422a139e6da20f1a9b
pkpete <pkpete@naver.com>
2021-08-15 11:37:28 +0900

    10773 제로.cpp

---
+++ 백준 알고리즘/10773 제로.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main() {
+	int k;
+	cin >> k;
+	stack<int> st;
+	for (int i = 0; i < k; i++) {
+		int tmp;
+		cin >> tmp;
+		if (tmp == 0)
+			st.pop();
+		else {
+			st.push(tmp);
+		}
+	}
+	int sum = 0;
+	while (!st.empty()) {
+		sum += st.top();
+		st.pop();
+	}
+	cout << sum;
+}
